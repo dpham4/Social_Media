040b97e010c61e9954ad8b941c62ddeb19f8ec22
dpham4 <dpham4@binghamton.edu>
2022-07-28 00:23:54 +0700

    Commiting files

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ProfileSide } from "../../components";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <ProfileSide />
+      <div className="postSide">Posts</div>
+      <div className="rightSide">Rightside</div>
+    </div>
+  );
+};
+
+export default Home;
